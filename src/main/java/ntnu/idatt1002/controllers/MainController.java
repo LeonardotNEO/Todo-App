f281ab4bfed2cafc60cee74c05958f84009f8564
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-03-12 12:39:02 +0100

    Tweaks

--- src/main/java/ntnu/idatt1002/controllers/MainController.java
+++ src/main/java/ntnu/idatt1002/controllers/MainController.java
@@ -1,13 +1,32 @@
 package ntnu.idatt1002.controllers;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import ntnu.idatt1002.App;
 
 import java.io.IOException;
+import java.security.cert.PolicyNode;
 
 public class MainController {
 
-    public void buttonLogout(ActionEvent event) throws IOException {
-        App.setRoot("login");
+
+    @FXML private AnchorPane navbar;
+    @FXML private AnchorPane content;
+
+    public void initialize() throws IOException {
+        setNavbar("navbar");
+    }
+
+    public void setMainContent(String fxmlfileName) throws IOException {
+        AnchorPane newContent =  FXMLLoader.load(getClass().getResource("/fxml/" + fxmlfileName + ".fxml"));
+        content.getChildren().setAll(newContent);
+        System.out.println("hello");
+        System.out.println(content.getChildren());
+    }
+
+    public void setNavbar(String fxmlfileName) throws IOException {
+        AnchorPane newContent =  FXMLLoader.load(getClass().getResource("/fxml/" + fxmlfileName + ".fxml"));
+        navbar.getChildren().setAll(newContent);
     }
 }
