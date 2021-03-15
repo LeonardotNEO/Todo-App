8fdba8df9db8bcab25d0cdde2b929c8b547f7452
Markus Solli Pedersen <markussp@stud.ntnu.no>
2021-03-15 13:32:53 +0100

    Added getUsers() in UserDAO

--- src/main/java/ntnu/idatt1002/dao/UserDAO.java
+++ src/main/java/ntnu/idatt1002/dao/UserDAO.java
@@ -1,11 +1,78 @@
 package ntnu.idatt1002.dao;
 
+import ntnu.idatt1002.User;
+
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.io.*;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
+import java.util.ArrayList;
 
 public final class UserDAO {
+    private static final String SAVEPATH = "resources/saves/";
+    private static final String FILETYPE = ".ser";
+
+    /**
+     * Get all users in storage
+     * @return an {@code ArrayList} of {@code User} objects
+     */
+    public static ArrayList<User> getUsers(){
+        ArrayList<User> users = new ArrayList<>(16);
+        File saveDirectory = new File(SAVEPATH);
+        String[] pathnames = saveDirectory.list();
+
+        if(pathnames != null){
+            for(String path : pathnames){
+                User user = deserializeUser(path);
+                users.add(user);
+            }
+        }
+
+        return users;
+    }
+
+    /**
+     * Save user to storage
+     * @param user {@code User} object
+     */
+    private static void serializeUser(User user){
+        File file = new File(SAVEPATH + user.getUsername() + FILETYPE);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(user);
+
+            oos.close();
+            fos.close();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * Get user object from storage
+     * @param username case-sensitive username
+     * @return {@code null} if error occurs
+     */
+    private static User deserializeUser(String username){
+        File file = new File(SAVEPATH + username + FILETYPE);
+        User user = null;
+        try{
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            user = (User) ois.readObject();
+
+            ois.close();
+            fis.close();
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return user;
+    }
+
     /**
      * @return a random salt
      */
