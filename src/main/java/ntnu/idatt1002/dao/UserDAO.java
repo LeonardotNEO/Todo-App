9847ba64777b4d9d1ec16cddc951c214345668f0
Markus Solli Pedersen <markussp@stud.ntnu.no>
2021-03-15 11:39:46 +0100

    Added UserDAO with methods for hashing

---
+++ src/main/java/ntnu/idatt1002/dao/UserDAO.java
@@ -0,0 +1,43 @@
+package ntnu.idatt1002.dao;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+
+public class UserDAO {
+    /**
+     * @return a random salt
+     */
+    public byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    /**
+     * PBKDF2 method to hash a password with salt
+     * @param password password in clear-text
+     * @param salt a users personal salt
+     * @return {@code null} if an error occured
+     */
+    public String hashPassword(String password, byte[] salt){
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+
+            //Convert byte[] to String
+            StringBuilder stringBuilder = new StringBuilder();
+            for(byte iByte : hash){
+                stringBuilder.append(Integer.toString((iByte & 0xff) + 0x100, 16).substring(1));
+            }
+
+            return stringBuilder.toString();
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
