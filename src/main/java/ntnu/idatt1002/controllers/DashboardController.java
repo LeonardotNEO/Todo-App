277aed64b9731a3eb51610e446376424e9658d17
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-03-15 10:26:10 +0100

    addCategory (fxml, controller and testclass), addTask (fxml, controller and testclass), editCategory (fxml, controller and testclass), NotificationHistoryControllerTest.java added

--- src/main/java/ntnu/idatt1002/controllers/DashboardController.java
+++ src/main/java/ntnu/idatt1002/controllers/DashboardController.java
@@ -1,8 +1,23 @@
 package ntnu.idatt1002.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class DashboardController {
 
-    public void addTaskButton(){
+    @FXML private Text categoryName;
+    @FXML private VBox categories;
+
+    public void buttonNewTask(){
+
+    }
+
+    public void buttonEditCategory(){
+
+    }
+
+    public void buttonNewCategory(){
 
     }
 }
--- src/test/java/controllers/DashBoardControllerTest.java
+++ src/test/java/controllers/DashBoardControllerTest.java
@@ -1,10 +1,5 @@
 package controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.text.Text;
-
 public class DashBoardControllerTest {
-    @FXML private Text categoryName;
-
 
 }
