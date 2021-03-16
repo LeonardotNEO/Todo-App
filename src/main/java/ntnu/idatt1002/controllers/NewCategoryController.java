59b78c565ff0ef43bc8620106369b1ebb244745f
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-03-16 17:01:17 +0100

    Updated editTask, newTask, editCategory, newCategory

--- src/main/java/ntnu/idatt1002/controllers/NewCategoryController.java
+++ src/main/java/ntnu/idatt1002/controllers/NewCategoryController.java
@@ -1,10 +1,14 @@
 package ntnu.idatt1002.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class NewCategoryController {
+
+    @FXML private TextField titleTextField;
 
     /**
      * cancel button loads the tasks page back into center-content of dashboard
@@ -16,6 +20,12 @@ public class NewCategoryController {
     }
 
     public void buttonNewCategory(ActionEvent event) throws IOException {
-        DashboardController.getInstance().setCenterContent("tasks");
+        boolean newCategory = true; // communicates with dao. Method for adding new category. Parameters are titleTextField.getText();
+
+        if(newCategory){
+            DashboardController.getInstance().setCenterContent("tasks");
+        } else {
+            // errormessage
+        }
     }
 }
