983d1d83ac9130352afa688338ac9824aa1b5cdd
Johannes <johstraa@stud.ntnu.no>
2021-03-16 22:08:59 +0100

    User and Category classes ready for push

--- src/main/java/ntnu/idatt1002/Category.java
+++ src/main/java/ntnu/idatt1002/Category.java
@@ -1,2 +1,59 @@
-package ntnu.idatt1002;public class Category {
+package ntnu.idatt1002;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Category {
+    private String name;
+    private ArrayList<Task> taskList;
+
+    public Category(String name) {
+        this.name = name;
+
+    }public String getName() {
+        return name;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public void addTaskList(ArrayList<Task> taskList) {
+        for (Task task: taskList) {
+            this.taskList.add(task);
+        }
+    }
+
+    public void removeTask(Task task) {
+        taskList.remove(taskList.indexOf(task));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return name.equals(category.name) && Objects.equals(taskList, category.taskList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, taskList);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", taskList=" + taskList +
+                '}';
+    }
 }
--- src/main/java/ntnu/idatt1002/User.java
+++ src/main/java/ntnu/idatt1002/User.java
@@ -1,2 +1,43 @@
-package ntnu.idatt1002;public class User {
+package ntnu.idatt1002;
+
+public class User {
+    private String username;
+    private String password;
+    private byte[] salt;
+
+    public User(){}
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(String username, String password, byte[] salt){
+        this.username = username;
+        this.password = password;
+        this.salt = salt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
 }
