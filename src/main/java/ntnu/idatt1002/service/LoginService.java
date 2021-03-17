b3943e16149a6dc4da25751b80d2ec6c0daeddfc
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-03-17 11:01:00 +0100

    Tweaks

--- src/main/java/ntnu/idatt1002/service/LoginService.java
+++ src/main/java/ntnu/idatt1002/service/LoginService.java
@@ -19,18 +19,15 @@ public class LoginService {
     }
 
     public static boolean checkIfLoginValid(String username, String password){
-        ArrayList<User> users = UserDAO.getUsers();
-        AtomicBoolean result = new AtomicBoolean(false);
+        boolean result = false;
+        User user = UserDAO.deserializeUser(username);
 
-        users.forEach(user -> {
-            if(user.getUsername().equals(username) && user.getPassword().equals(password)){
-                result.set(true);
-                System.out.println(user.getUsername().equals(username) + " " + user.getPassword().equals(password));
-            }
-        });
+        if(user.getPassword().equals(password)){
+            System.out.println(user.getUsername().equals(username) + " " + user.getPassword().equals(password));
+            result = true;
+        }
 
-
-        return result.get();
+        return result;
     }
 
     public static void saveLogin(){
