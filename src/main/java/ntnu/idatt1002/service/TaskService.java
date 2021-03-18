dd2733010d7d6e384a12eb6c340bbbec21d78939
Leonard <leonaro@stud.ntnu.no>
2021-03-18 14:45:53 +0100

    NewTaskService to TaskService

--- src/main/java/ntnu/idatt1002/service/TaskService.java
+++ src/main/java/ntnu/idatt1002/service/TaskService.java
@@ -3,7 +3,7 @@ package ntnu.idatt1002.service;
 import ntnu.idatt1002.Task;
 import ntnu.idatt1002.dao.TaskDAO;
 
-public class NewTaskService {
+public class TaskService {
 
     public static boolean newTask(String username, String title, String deadline, String description, int priority, String startDate) {
         Task newTask = new Task(title, username, description, deadline, priority, startDate);
