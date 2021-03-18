9812d96438f94424832ba0869ad785d94950b862
Leonard <leonaro@stud.ntnu.no>
2021-03-18 17:12:13 +0100

    Update to notification

--- src/main/java/ntnu/idatt1002/service/NotificationService.java
+++ src/main/java/ntnu/idatt1002/service/NotificationService.java
@@ -1,6 +1,7 @@
 package ntnu.idatt1002.service;
 import ntnu.idatt1002.Notification;
 import ntnu.idatt1002.dao.NotificationDAO;
+import ntnu.idatt1002.dao.UserStateDAO;
 
 import java.util.ArrayList;
 
@@ -10,5 +11,10 @@ public class NotificationService {
         return NotificationDAO.getNotifsByUser(username);
     }
 
+    public static void newNotification(String title, String description){
+        Notification notification = new Notification(title, UserStateService.getCurrentUser().getUsername(), description);
+
+        NotificationDAO.serializeNotif(notification);
+    }
 
 }
