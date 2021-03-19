af375488ab69ac19d9fa1b96d21263348208bfc8
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-03-19 10:49:39 +0100

    When navigation bar is initiliazed, notifications are loaded into menuButton for notifications

--- src/main/java/ntnu/idatt1002/controllers/NavbarController.java
+++ src/main/java/ntnu/idatt1002/controllers/NavbarController.java
@@ -1,12 +1,44 @@
 package ntnu.idatt1002.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import ntnu.idatt1002.App;
+import ntnu.idatt1002.service.NotificationService;
 import ntnu.idatt1002.service.UserStateService;
+
 
 import java.io.IOException;
 
 public class NavbarController {
+
+    @FXML private MenuButton notificationBell;
+    @FXML private Button buttonNotificationHistory;
+
+    public void initialize(){
+        Button newButton = buttonNotificationHistory;
+        notificationBell.getItems().add(new CustomMenuItem(newButton));
+
+        NotificationService.getNotCheckedNotifications().forEach(notification -> {
+            try {
+                Pane element = FXMLLoader.load(getClass().getResource("/fxml/notificationElement.fxml"));
+                element.setId(Integer.toString(notification.hashCode()));
+
+                Text title = (Text) element.lookup("#title");
+                title.setText(notification.getTitle());
+
+                Label description = (Label) element.lookup("#description");
+                description.setText(notification.getDescription());
+
+                notificationBell.getItems().add(new CustomMenuItem(element));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
     /**
      * Communicate with mainController to updates its main-content to dashboard.fxml when button in navbar is clicked
@@ -27,12 +59,16 @@ public class NavbarController {
     }
 
     /**
-     * Communicate with mainController to updates its main-content to notificationHistory.fxml when button on notifcations is clicked
+     * Communicate with mainController to updates its main-content to notificationHistory.fxml when button in notificationBell-menu is clicked
      * @param event
      * @throws IOException
      */
-    public void buttonNotificationHistory(ActionEvent event) throws IOException{
+    public void buttonNotificationHistory(ActionEvent event) throws IOException {
         MainController.getInstance().setMainContent("notificationHistory");
+    }
+
+    public void notificationBellClicked(ActionEvent event) throws IOException {
+        System.out.println("clicked");
     }
 
     /**
