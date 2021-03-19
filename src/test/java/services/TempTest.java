e50d80d4492113b8d611f9f8d6c06c28e1c83237
saschrad <sanderhegland@gmail.com>
2021-03-19 16:29:57 +0100

    Testing

---
+++ src/test/java/services/TempTest.java
@@ -0,0 +1,16 @@
+import ntnu.idatt1002.Task;
+import ntnu.idatt1002.service.TaskService;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TempTest {
+
+    @Test
+    public void test() {
+        ArrayList<Task> list = TaskService.TaskSortedByPriority();
+        for(Task a: list) {
+            System.out.println(a);
+        }
+    }
+}
