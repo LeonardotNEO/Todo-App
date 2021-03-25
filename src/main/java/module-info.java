6ae725ad7ed77ce3d0f56645e9c864c5a6ce337d
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-03-25 08:47:09 +0100

    Adde ntnu.idatt1002 access to com.jfoenix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module ntnu.idatt1002 {
     requires de.jensd.fx.glyphs.commons;
     requires de.jensd.fx.glyphs.materialdesignicons;
     requires de.jensd.fx.glyphs.fontawesome;
+    requires com.jfoenix;
 
     exports ntnu.idatt1002;
     opens ntnu.idatt1002.controllers to javafx.fxml;
