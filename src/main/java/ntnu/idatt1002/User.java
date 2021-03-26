8ae6dc0d0f32dd91041b7cb7b3b026e63b934173
Håkon Røsakft <haakoero@stud.ntnu.no>
2021-03-26 16:32:31 +0100

    Added javadoc to the classes in the main package

--- src/main/java/ntnu/idatt1002/User.java
+++ src/main/java/ntnu/idatt1002/User.java
@@ -3,6 +3,10 @@ package ntnu.idatt1002;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * A class which functions as a user of the application
+ * This class contains the basic method of any given user
+ */
 public class User implements Serializable {
     private String username;
     private String password;
@@ -10,40 +14,79 @@ public class User implements Serializable {
 
     public User(){}
 
+    /**
+     * A constructor for the user class which only needs a username
+     * @param username
+     */
     public User(String username) {
         this.username = username;
     }
 
+    /**
+     * A constructor for the user class which needs all the necessary information on a user
+     * @param username
+     * @param password
+     * @param salt
+     */
     public User(String username, String password, byte[] salt){
         this.username = username;
         this.password = password;
         this.salt = salt;
     }
 
+    /**
+     * A method to get the username
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * A method to set the username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * A method to get the password
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * A method to set the password
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * A method to get the salt
+     * @return the salt
+     */
     public byte[] getSalt() {
         return salt;
     }
 
+    /**
+     * A method to set the salt
+     * @param salt
+     */
     public void setSalt(byte[] salt) {
         this.salt = salt;
     }
 
+    /**
+     * A equals method which compares a objects content with this user-object
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,6 +95,10 @@ public class User implements Serializable {
         return username.equals(user.username);
     }
 
+    /**
+     * A method which returns hashCode of the user object
+     * @return
+     */
     @Override
     public int hashCode() {
         return Objects.hash(username);
