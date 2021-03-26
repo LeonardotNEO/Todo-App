2238bf02f4efc96bda6e557689233d13ea617d4f
Håkon Røsakft <haakoero@stud.ntnu.no>
2021-03-26 16:18:47 +0100

    Added javadoc to the class DateConverter and its methods

--- src/main/java/ntnu/idatt1002/utils/DateConverter.java
+++ src/main/java/ntnu/idatt1002/utils/DateConverter.java
@@ -5,18 +5,33 @@ import javafx.util.StringConverter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A class which provides methods to convert a date
+ */
 public class DateConverter extends StringConverter<LocalDate> {
     DateTimeFormatter dateFormatter;
 
+    /**
+     * A method which sets the format of the DateConverter to (dd/MM/yyyy)
+     */
     public DateConverter () {
         dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     }
 
+    /**
+     * A method to set the format for the date converter
+     * @param format
+     */
     // This should be checked to be valid / it throws invalid argument exception if pattern is invalid.
     public DateConverter(String format) {
         dateFormatter = DateTimeFormatter.ofPattern(format);
     }
 
+    /**
+     * A method to turn a date into a string
+     * @param date
+     * @return
+     */
     @Override
     public String toString(LocalDate date) {
         if(date != null) {
@@ -26,6 +41,11 @@ public class DateConverter extends StringConverter<LocalDate> {
         }
     }
 
+    /**
+     * A method to turn a string into a date
+     * @param string
+     * @return
+     */
     @Override
     public LocalDate fromString(String string) {
         if(string != null && !string.isEmpty()){
