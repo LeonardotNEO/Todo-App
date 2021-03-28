7391c1b1fc119a25eb697789d4f6d82276783014
Markus Solli Pedersen <markussp@stud.ntnu.no>
2021-03-28 18:43:34 +0200

    Created functions for writing to logfile

--- src/main/java/ntnu/idatt1002/dao/UserLogDAO.java
+++ src/main/java/ntnu/idatt1002/dao/UserLogDAO.java
@@ -2,6 +2,8 @@ package ntnu.idatt1002.dao;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Static class to store actions in a user log file
@@ -12,7 +14,27 @@ public final class UserLogDAO {
     private static final String FILENAME = "userlog.txt";
 
     public static void setUserRegistration(String username){
+        writeEntry(username, "User created");
+    }
 
+    public static void setCategoryAdded(String username, String category){
+        writeEntry(username, "Category created: " + category);
+    }
+
+    public static void setCategoryRemoved(String username, String category){
+        writeEntry(username, "Category removed: " + category);
+    }
+
+    public static void setTaskAdded(String username, String title){
+        writeEntry(username, "Task created: " + title);
+    }
+
+    public static void setTaskRemoved(String username, String title){
+        writeEntry(username, "Task removed: " + title);
+    }
+
+    public static void setTaskDone(String username, String title){
+        writeEntry(username, "Task marked as done: " + title);
     }
 
     /**
@@ -21,9 +43,14 @@ public final class UserLogDAO {
      * @param entry String containing datetime, entry type and entry message
      */
     private static void writeEntry(String username, String entry){
+        //Get current datetime and format
+        LocalDateTime datetime = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatDatetime = datetime.format(format);
+
         try {
             FileWriter fileWriter = new FileWriter(filePath(username));
-            fileWriter.write(entry);
+            fileWriter.write(formatDatetime + " - " + entry + ".\n");
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
