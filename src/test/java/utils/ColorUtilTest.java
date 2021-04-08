137483f390682f7d114fb0783c0f5a8e44a33f79
saschrad <sanderhegland@gmail.com>
2021-04-08 21:23:25 +0200

    Made tests use assertions instead of sout

--- src/test/java/utils/ColorUtilTest.java
+++ src/test/java/utils/ColorUtilTest.java
@@ -5,40 +5,50 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.Color;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ColorUtilTest {
 
     @Test
     public void testHexToRgb(){
-        System.out.println(ColorUtil.hexToRgb("#001021"));
+        Color color = ColorUtil.hexToRgb("#001021");
+        int[] expectedColors = {0, 16, 33};
+        int[] actualColors = {color.getRed(), color.getGreen(), color.getBlue()};
+        assertArrayEquals(expectedColors,actualColors);
     }
 
     @Test
     public void getHSBfromHEX(){
-        Color rgb = ColorUtil.hexToRgb("#001021");
-        System.out.println(rgb);
-
-        float[] hsb = ColorUtil.RgbToHsb(rgb);
-        System.out.println(hsb[0]);
-        System.out.println(hsb[1]);
-        System.out.println(hsb[2]);
+        Color rgb = ColorUtil.hexToRgb("#000000");
+        float[] hsbExpected = {0, 0, 0};
+        float[] hsbActual = ColorUtil.RgbToHsb(rgb);
+        assertArrayEquals(hsbExpected, hsbActual);
     }
 
     @Test
     public void getBrightnessFromHex(){
-        System.out.println(ColorUtil.getHexBrightness("#001021"));
+        int brightnessWhite = ColorUtil.getHexBrightness("#FFFFFF");
+        int brightnessBlack = ColorUtil.getHexBrightness("#000000");
+        assertEquals(0, brightnessBlack);
+        assertEquals(100, brightnessWhite);
     }
 
     @Test
     public void getSaturationFromHex(){
-        System.out.println(ColorUtil.getHexSaturation("#2b5b8f"));
-        System.out.println(ColorUtil.getHexSaturationInverted("#2b5b8f"));
+        int saturationBlack = ColorUtil.getHexSaturation("#000000");
+        int saturationBlackInverted = ColorUtil.getHexSaturationInverted("#000000");
+        assertEquals(0, saturationBlack);
+        assertEquals(100, saturationBlackInverted);
     }
 
     @Test
     public void checkTextColorWithHexBackground(){
-        String hexcolor = "#f5a9a9";
-        System.out.println(ColorUtil.getHexBrightness(hexcolor));
-        System.out.println(ColorUtil.getHexSaturation(hexcolor));
-        System.out.println("value: " + ColorUtil.getVisiblityRating(hexcolor));
+        String hexcolor = "#FFFFFF";
+        // Max brightness
+        assertEquals(100, ColorUtil.getHexBrightness(hexcolor));
+        // No saturation
+        assertEquals(0, ColorUtil.getHexSaturation(hexcolor));
+        // Highest contrast -> black
+        assertEquals(0, ColorUtil.getVisiblityRating(hexcolor));
     }
 }
