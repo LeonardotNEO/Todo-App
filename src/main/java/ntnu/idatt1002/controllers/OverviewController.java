010b9fc5e790d6b1cb1ed2c8688dcea98135407b
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-04-08 15:04:34 +0200

    Added calender to overview page

--- src/main/java/ntnu/idatt1002/controllers/OverviewController.java
+++ src/main/java/ntnu/idatt1002/controllers/OverviewController.java
@@ -7,14 +7,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import ntnu.idatt1002.Task;
 import ntnu.idatt1002.service.TaskService;
 import ntnu.idatt1002.utils.DateUtils;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.temporal.*;
 import java.util.ArrayList;
+import java.util.Currency;
+import java.util.HashMap;
 
 public class OverviewController {
 
@@ -22,26 +30,107 @@ public class OverviewController {
     @FXML private VBox dueNext7Days;
     @FXML private VBox dueThisMonth;
     @FXML private ScrollPane scrollpane;
-    @FXML private HBox simpleView;
 
-    @FXML private AnchorPane calenderView;
+    @FXML private GridPane calenderView;
 
     @FXML private AnchorPane content;
 
+    /**
+     * Initialize method used for loading the SimpleView as default when loading "Overview"
+     * @throws IOException
+     */
     public void initialize() throws IOException {
+        initializeSimpleView();
+    }
+
+    /**
+     * Method used for loading SimpleView
+     * @throws IOException
+     */
+    public void initializeSimpleView() throws IOException {
+        // clean simpleview before adding new task UI elements
+        removeTasksFromSimpleView();
+
+        // display this node
+        displayNode("simpleView");
+
+        // add listeners to scrollpanes (for resizing)
         addScrollpaneListener();
 
-        addTasksToSimpleView("dueThisWeek", getTaskUIListFromTaskObjectList(TaskService.getTasksByCategory("Finished tasks")));
-        addTasksToSimpleView("dueNext7Days", getTaskUIListFromTaskObjectList(TaskService.getTasksByCategory("Trash bin")));
-        addTasksToSimpleView("dueThisMonth", getTaskUIListFromTaskObjectList(TaskService.getTasksByCurrentUser()));
+        // Time now
+        LocalDateTime now = LocalDateTime.now();
+
+        // The remaining days of the current week
+        LocalDateTime firstDayOfNextWeek = now.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+        LocalDateTime nowPlusDaysUntilNextMonday = now.plusDays(ChronoUnit.DAYS.between(now, firstDayOfNextWeek));
+        addTasksToSimpleView("dueThisWeek", getTaskUIListFromTaskObjectList(TaskService.getTasksBetweenDates(TaskService.getTasksByCurrentUser(), DateUtils.getAsMs(now), DateUtils.getAsMs(nowPlusDaysUntilNextMonday))));
+
+        // The next 7 days date
+        LocalDateTime nowPlus7Days = now.plusDays(7);
+        addTasksToSimpleView("dueNext7Days", getTaskUIListFromTaskObjectList(TaskService.getTasksBetweenDates(TaskService.getTasksByCurrentUser(), DateUtils.getAsMs(now), DateUtils.getAsMs(nowPlus7Days))));
+
+        // The remaining days of the current month
+        LocalDateTime endOfMonthDate = now.with(TemporalAdjusters.lastDayOfMonth());
+        addTasksToSimpleView("dueThisMonth", getTaskUIListFromTaskObjectList(TaskService.getTasksBetweenDates(TaskService.getTasksByCurrentUser(), DateUtils.getAsMs(now), DateUtils.getAsMs(endOfMonthDate))));
     }
 
-    public void buttonSimpleView() {
-        displayNode("simpleView");
-    }
-
-    public void buttonCalenderView() {
+    /**
+     * Method used for loading CalenderView
+     */
+    public void initializeCalenderView(){
+        // display this node
         displayNode("calenderView");
+
+        // Time now
+        LocalDateTime currentDate = LocalDateTime.now().with(TemporalAdjusters.firstDayOfMonth());
+
+        int startWeek = currentDate.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+        int currentWeek = -1;
+        int daysInMonth = YearMonth.of(currentDate.getYear(), currentDate.getMonthValue()).lengthOfMonth();
+        int row = 1;
+
+        HashMap<String, Integer> daysColumns = new HashMap<>() {{
+            put("MONDAY", 1);
+            put("TUESDAY", 2);
+            put("WEDNESDAY", 3);
+            put("THURSDAY", 4);
+            put("FRIDAY", 5);
+            put("SATURDAY", 6);
+            put("SUNDAY", 7);
+        }};
+
+        for(int i = 0; i < daysInMonth; i++){
+            System.out.println(currentDate.getDayOfWeek().getValue());
+
+            calenderView.add(new Text("Day: " + currentDate.get(ChronoField.DAY_OF_MONTH)), daysColumns.get(currentDate.getDayOfWeek().toString()), row);
+
+            if(currentWeek != currentDate.get(ChronoField.ALIGNED_WEEK_OF_YEAR)){
+                currentWeek = currentDate.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+                calenderView.add(new Text("week: " + currentWeek), 0,row);
+            }
+
+            if(currentDate.getDayOfWeek().getValue() == 7){
+                row++;
+            }
+
+            currentDate = currentDate.plusDays(1);
+        }
+    }
+
+    /**
+     * Pressing SimpleView button in overview-navbar
+     * @throws IOException
+     */
+    public void buttonSimpleView() throws IOException {
+        initializeSimpleView();
+    }
+
+    /**
+     * Pressing CalenderView button in overview-navbar
+     * @throws IOException
+     */
+    public void buttonCalenderView() throws IOException {
+        initializeCalenderView();
     }
 
     /**
@@ -52,10 +141,8 @@ public class OverviewController {
         for(Node node : content.getChildren()){
             if(node.getId().equals(nodeId)){
                 node.setVisible(true);
-                node.setVisible(true);
             } else {
                 node.setVisible(false);
-                node.setManaged(false);
             }
         }
     }
@@ -72,7 +159,7 @@ public class OverviewController {
                     dueThisWeek.getChildren().add(dueThisWeek.getChildren().size(), task);
                 }
                 break;
-            case "dueNextMonth":
+            case "dueNext7Days":
                 for(AnchorPane task : taskUI) {
                     dueNext7Days.getChildren().add(dueNext7Days.getChildren().size(), task);
                 }
@@ -85,6 +172,15 @@ public class OverviewController {
             default:
                 break;
         }
+    }
+
+    /**
+     * Method used for removing UI elements from dueThisWeek VBox, dueNext7Days VBox and dueThisMonth VBox
+     */
+    public void removeTasksFromSimpleView(){
+        dueThisWeek.getChildren().removeAll(dueThisWeek.getChildren());
+        dueNext7Days.getChildren().removeAll(dueNext7Days.getChildren());
+        dueThisMonth.getChildren().removeAll(dueThisMonth.getChildren());
     }
 
     /**
