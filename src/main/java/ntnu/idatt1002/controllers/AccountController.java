76270df5313518b9a861bf1bb809f72a240edb9e
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2021-04-09 12:35:32 +0200

    Updated accountpage to show stats in information, and ability to edit user with new username or password

--- src/main/java/ntnu/idatt1002/controllers/AccountController.java
+++ src/main/java/ntnu/idatt1002/controllers/AccountController.java
@@ -2,42 +2,145 @@ package ntnu.idatt1002.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
-import ntnu.idatt1002.service.UserStateService;
+import ntnu.idatt1002.User;
+import ntnu.idatt1002.dao.UserDAO;
+import ntnu.idatt1002.service.*;
+import ntnu.idatt1002.utils.DateUtils;
 
 /**
  * A class which contains the buttons related to a user account
  */
 public class AccountController {
 
+    // information
     @FXML private Text username;
     @FXML private Text dateCreated;
     @FXML private Text taskCount;
     @FXML private Text categoryCount;
+    @FXML private VBox information;
 
+    // edit account
+    @FXML private VBox editUserPage;
+    @FXML private TextField editUsername;
+    @FXML private TextField editPassword;
+    @FXML private TextField editRepeatPassword;
+    @FXML private Label errorMessage;
 
+    // general
+    @FXML private AnchorPane content;
+    @FXML private HBox bottomButtons;
+
+    /**
+     * When accountcontroller is initialized, we load the information page
+     */
     public void initialize(){
-        // when page is initialized, set text fields to appropriate values
+        // load information page
+        showInformationPage();
+    }
+
+    /**
+     * Method runs when confirmEditUser is clicked.
+     * Check if username and password syntax is valid, and display errormessage.
+     * Display successful message if successful.
+     * @param event
+     */
+    public void confirmEditUser(ActionEvent event){
+        User user = UserStateService.getCurrentUser();
+        String errorMessageString = "";
+
+        // username
+        if(RegisterService.checkIfUsernameValid(editUsername.getText()) || UserStateService.getCurrentUserUsername().equals(editUsername.getText())){
+            user.setUsername(editUsername.getText());
+        } else {
+            errorMessageString += "That username is not available \n";
+        }
+        if(!RegisterService.checkIfUsernameValidSyntax(editUsername.getText())){
+            errorMessageString += "Username cant be empty \n";
+        }
+
+        // password
+        if(RegisterService.checkIfPasswordValidSyntax(editPassword.getText(), editRepeatPassword.getText())){
+            user.setPassword(editPassword.getText());
+        } else {
+            errorMessageString += "Password length must be more than 6 characters \n";
+        }
+        if (!RegisterService.checkIfPasswordValid(editPassword.getText(), editRepeatPassword.getText())){
+            errorMessageString += "Password do not match \n";
+        }
+
+        if(errorMessageString.isEmpty()){
+            UserService.editUser(UserStateService.getCurrentUser(),user);
+            errorMessage.setText("Account successfully edited!");
+            errorMessage.setTextFill(Paint.valueOf("green"));
+        } else {
+            errorMessage.setText(errorMessageString);
+            errorMessage.setTextFill(Paint.valueOf("red"));
+        }
+    }
+
+    /**
+     * Button editAccount in information page
+     * @param event
+     */
+    public void buttonEditUser(ActionEvent event){
+        showEditAccountPage();
+    }
+
+    /**
+     * Button cancel in edit account page
+     * @param event
+     */
+    public void buttonCancel(ActionEvent event){
+        showInformationPage();
+    }
+
+    /**
+     * Method used for showing the information page
+     */
+    public void showInformationPage(){
+        displayNode(content, "information");
+        displayNode(bottomButtons, "buttonEditUser");
+
         username.setText(UserStateService.getCurrentUserUsername());
-        dateCreated.setText("");
-        taskCount.setText("");
-        categoryCount.setText("");
+        dateCreated.setText(DateUtils.getFormattedDate(UserStateService.getCurrentUser().getDateCreated()));
+        taskCount.setText(Integer.toString(TaskService.getTasksByCurrentUser().size()));
+        categoryCount.setText(Integer.toString(CategoryService.getCategoriesCurrentUserWithoutPremades().size()));
     }
 
     /**
-     * Updates main content to resetPassword page
-     * @param event
+     * Method used for showing the edit account page
      */
-    public void resetPasswordButton(ActionEvent event){
+    public void showEditAccountPage(){
+        displayNode(content,"editUserPage");
+        displayNode(bottomButtons, "buttonCancel");
 
+        editUsername.setText(UserStateService.getCurrentUserUsername());
+        errorMessage.setText("");
     }
 
     /**
-     * Updates main content to resetEmail page
-     * @param event
+     * Display this page under anchorpane content in account page
+     * @param nodeId
      */
-    public void resetEmailButton(ActionEvent event){
-
+    public void displayNode(Parent parentNode, String nodeId){
+        for(Node node : parentNode.getChildrenUnmodifiable()){
+            if(node.getId().equals(nodeId)){
+                node.setVisible(true);
+                node.setManaged(true);
+            } else {
+                node.setVisible(false);
+                node.setManaged(false);
+            }
+        }
     }
 
 }
