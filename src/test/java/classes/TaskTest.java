fbf86f1671e6774d6533e1da070aa22bac4556fe
unknown <sanderhegland@gmail.com>
2021-04-14 15:34:30 +0200

    Added test for TaskBuilder

--- src/test/java/classes/TaskTest.java
+++ src/test/java/classes/TaskTest.java
@@ -13,17 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TaskTest {
     @Test
     public void createTask() {
-        long dead = DateUtils.getAsMs(LocalDate.of(2022, 02, 20));
-        Task b = new Task.TaskBuilder("admin", "Test61001")
-                .description("321832913291")
-                .deadline(dead)
-                .priority(1)
-                .startDate(1l)
-                .category("home")
-                .build();
-
-        System.out.println(b);
-
         Task t = new Task.TaskBuilder("admin", "This is the task title")
                 .category("test")
                 .build();
