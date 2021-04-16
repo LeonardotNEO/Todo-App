0421b64c2c220f0206b2f63bc28e5a4062529ee7
unknown <sanderhegland@gmail.com>
2021-04-16 14:21:12 +0200

    Added json simple to module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module ntnu.idatt1002 {
     requires de.jensd.fx.glyphs.fontawesome;
     requires com.jfoenix;
     requires java.desktop;
+    requires json.simple;
 
     exports ntnu.idatt1002;
     opens ntnu.idatt1002.controllers to javafx.fxml;
