15f0e81af313c84a342b7fd9b566dc56e9e2277d
unknown <sanderhegland@gmail.com>
2021-04-16 16:09:56 +0200

    Added classes so the dao clan can create objects from them and created getters

--- src/main/java/ntnu/idatt1002/HelpSection.java
+++ src/main/java/ntnu/idatt1002/HelpSection.java
@@ -1,2 +1,44 @@
-package ntnu.idatt1002;public class HelpSection {
+package ntnu.idatt1002;
+
+
+import java.util.ArrayList;
+
+public class HelpSection {
+    public String section;
+    public String description;
+    public ArrayList<Info> fields;
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ArrayList<Info> getFields() {
+        return fields;
+    }
+
+    public class Info {
+        public String image;
+        public String text;
+
+        @Override
+        public String toString() {
+            return "Info{" +
+                    "image='" + image + '\'' +
+                    ", text='" + text + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HelpSection{" +
+                "section='" + section + '\'' +
+                ", description='" + description + '\'' +
+                ", fields=" + fields.toString() +
+                '}';
+    }
 }
