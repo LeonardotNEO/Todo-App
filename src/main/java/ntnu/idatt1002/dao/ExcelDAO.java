d337451c53e239d44e74c1e4def3154fc19db7e6
Markus Solli Pedersen <markussp@stud.ntnu.no>
2021-04-17 16:13:46 +0200

    Write function now prints all tasks to csv

--- src/main/java/ntnu/idatt1002/dao/ExcelDAO.java
+++ src/main/java/ntnu/idatt1002/dao/ExcelDAO.java
@@ -1,27 +1,80 @@
 package ntnu.idatt1002.dao;
 
+import ntnu.idatt1002.Task;
+import ntnu.idatt1002.User;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
 
+/**
+ * Class for writing user data to csv files and retrieving the generated files
+ */
 public final class ExcelDAO {
     private static final String SAVEPATH = "src/main/resources/saves";
+    private static final String[] HEADERS = {
+            "name",
+            "project",
+            "category",
+            "description",
+            "priority",
+            "start date",
+            "deadline",
+            "location"
+    };
 
-    public static void export(String username) throws IOException {
-        FileWriter fileWriter = new FileWriter(filepath(username));
-        CSVPrinter printer = new CSVPrinter(fileWriter, CSVFormat.DEFAULT.withHeader("username"));
-        printer.printRecord(username);
+    /**
+     * Export user data to a csv file under the user folder
+     * @param username {@code username} variable for a {@link User}
+     * @throws IOException if file could not be found
+     */
+    public static void write(String username) throws IOException {
+        if(UserDAO.exists(username)){
+            //Clear file
+            FileWriter fileWriter = new FileWriter(filepath(username), false);
 
-        printer.flush();
-        printer.close();
-        fileWriter.close();
+            //Create printer
+            CSVPrinter printer = new CSVPrinter(fileWriter, CSVFormat.DEFAULT.withHeader(HEADERS));
+
+            //Get content
+            ArrayList<Task> tasks = TaskDAO.list(username);
+
+            //Print content
+            if(tasks != null) {
+                for(Task task : tasks) {
+                    printer.printRecord(
+                            task.getName(),
+                            task.getProject(),
+                            task.getCategory(),
+                            task.getDescription(),
+                            task.getPriority(),
+                            task.getStartDate(),
+                            task.getDeadline(),
+                            task.getLocation()
+                    );
+                }
+            }
+
+            //Close printer
+            printer.flush();
+            printer.close();
+            fileWriter.close();
+        }
     }
 
+    /**
+     * Get user csv file
+     * @return a {@link File} object referencing the csv file, or {@code null} if file does not exist
+     */
+    public static File read(String username){
+        return null;
+    }
+
+    //Get paths
     private static String userDir(String username){
         return (SAVEPATH + "/" + username + "/");
     }
--- src/test/java/dao/ExcelDAOTest.java
+++ src/test/java/dao/ExcelDAOTest.java
@@ -1,21 +1,45 @@
 package dao;
 
+import ntnu.idatt1002.Task;
 import ntnu.idatt1002.User;
-import ntnu.idatt1002.dao.ExcelDAO;
-import ntnu.idatt1002.dao.UserDAO;
+import ntnu.idatt1002.dao.*;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ExcelDAOTest {
+    private final static Task taskA = new Task.TaskBuilder("olanormann", "Do laundry")
+            .description("Wash colored and wool")
+            .priority(1)
+            .category("Home")
+            .build();
+
+    private final static Task taskB = new Task.TaskBuilder("olanormann", "Build walls")
+            .description("All outer walls")
+            .priority(2)
+            .project("Build house")
+            .category("Carpentry")
+            .build();
+
     @BeforeAll
     public static void setup(){
         UserDAO.serialize(new User("olanormann"));
+        CategoryDAO.add("olanormann", "Home");
+        ProjectDAO.add("olanormann", "Build house");
+        CategoryDAO.add("olanormann", "Build house", "Carpentry");
+        TaskDAO.serialize(taskA);
+        TaskDAO.serialize(taskB);
     }
 
     @Test
     public void export(){
-        assertDoesNotThrow(() -> ExcelDAO.export("olanormann"));
+        assertDoesNotThrow(() -> ExcelDAO.write("olanormann"));
+    }
+
+    @AfterAll
+    public static void cleanup(){
+        TaskDAO.deleteByUser("olanormann");
     }
 }
