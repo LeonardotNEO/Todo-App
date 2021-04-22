10637473d8d6ded6f2fc51075da11975841e95d8
Markus Solli Pedersen <markussp@stud.ntnu.no>
2021-04-22 17:41:43 +0200

    Added methods for project and category handling to CommonDAO

--- src/main/java/ntnu/idatt1002/dao/CommonDAO.java
+++ src/main/java/ntnu/idatt1002/dao/CommonDAO.java
@@ -23,4 +23,79 @@ public final class CommonDAO {
     static void setTasks(HashMap<String, HashMap<String, ArrayList<Task>>> newTasks){
         tasks = newTasks;
     }
+
+    /**
+     * Add a standard category to the logged in user.
+     * @param category the category name.
+     */
+    public static void addCategory(String category){
+        tasks.get("Standard").put(category, new ArrayList<>());
+    }
+
+    /**
+     * Add a project category to the logged in user.
+     * @param project the already existing project name.
+     * @param category the category name.
+     */
+    public static void addCategory(String project, String category){
+        tasks.get(project).put(category, new ArrayList<>());
+    }
+
+    /**
+     * Add a project to the logged in user. Can not be called 'Standard'.
+     * @param project the project name.
+     */
+    public static void addProject(String project){
+        if(!project.equals("Standard")){ tasks.put(project, new HashMap<>()); }
+    }
+
+    /**
+     * Get an array of all projects for the logged in user.
+     * @return a {@link String}[] of project names.
+     */
+    public static String[] listProjects(){
+        return (String[]) tasks.keySet().toArray();
+    }
+
+    /**
+     * Get an array of all standard categories for the logged in user.
+     * @return a {@link String}[] of category names.
+     */
+    public static String[] listCategories(){
+        return (String[]) tasks.get("Standard").keySet().toArray();
+    }
+
+    /**
+     * Get an array of all categories within a project for the logged in user.
+     * @param project the project name.
+     * @return a {@link String}[] of category names.
+     */
+    public static String[] listCategories(String project){
+        return (String[]) tasks.get(project).keySet().toArray();
+    }
+
+    /**
+     * Delete a project from the logged in user. Can't delete 'Standard' project.
+     * @param project the project name.
+     */
+    public static void deleteProject(String project){
+        if(!project.equals("Standard")){ tasks.remove(project); }
+    }
+
+    /**
+     * Delete a standard category from the logged in user.
+     * @param category the category name.
+     */
+    public static void deleteCategory(String category){
+        tasks.get("Standard").remove(category);
+    }
+
+    /**
+     * Delete a project category from the logged in user.
+     * @param project the project name.
+     * @param category the category name.
+     */
+    public static void deleteCategory(String project, String category){
+        tasks.get(project).remove(category);
+    }
  }
