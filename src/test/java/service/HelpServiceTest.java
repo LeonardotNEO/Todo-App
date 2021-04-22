aaaea9276861510596fab888a8daf383de06bca9
saschrad <saschrad@stud.ntnu.no>
2021-04-22 17:17:49 +0200

    Added HelpServiceTests

---
+++ src/test/java/service/HelpServiceTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import ntnu.idatt1002.service.HelpService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelpServiceTest {
+
+    @Test
+    public void getSectionsTest() {
+        assertTrue(HelpService.getSections().size() > 1);
+    }
+
+    @Test
+    public void getSpecificSectionTest() {
+        String section = HelpService.getSections().get(0);
+        assertEquals(section, HelpService.getSection(section).getSection());
+    }
+}
