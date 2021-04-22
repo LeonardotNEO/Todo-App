2b5964721a7c8972b4a7c05f2a52c76b049eb949
Markus Solli Pedersen <markussp@stud.ntnu.no>
2021-04-22 16:39:36 +0200

    Added CommonDAO. Will provide same services as User-, Task-, Category- and Project-DAO.

---
+++ src/main/java/ntnu/idatt1002/dao/CommonDAO.java
@@ -0,0 +1,10 @@
+package ntnu.idatt1002.dao;
+
+import ntnu.idatt1002.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public final class CommonDAO {
+    private static HashMap<String, HashMap<String, ArrayList<Task>>> tasks = new HashMap<>();
+ }
