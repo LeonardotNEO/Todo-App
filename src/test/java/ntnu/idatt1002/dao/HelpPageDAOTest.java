5c889b8ddee3784a1435181fd90bbf13e9a2c182
saschrad <saschrad@stud.ntnu.no>
2021-04-27 16:59:00 +0200

    Added tests for HelpPageDAO

--- src/test/java/ntnu/idatt1002/dao/HelpPageDAOTest.java
+++ src/test/java/ntnu/idatt1002/dao/HelpPageDAOTest.java
@@ -1,15 +1,21 @@
 package ntnu.idatt1002.dao;
 
+import ntnu.idatt1002.model.HelpSection;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HelpPageDAOTest {
 
     @Test
-    public void kappa() {
-        HelpPageDAO.getData();
+    public void getDataTest() {
+        HelpSection[] helpSections = HelpPageDAO.getData();
+        assertEquals(helpSections[0].getSection(), "Register");
     }
+
+
     @Test
-    public void test() {
-        HelpPageDAO.getSections().forEach(System.out::println);
+    public void getSectionsTest() {
+        assertEquals(HelpPageDAO.getSections().get(0), "Register");
     }
 }
