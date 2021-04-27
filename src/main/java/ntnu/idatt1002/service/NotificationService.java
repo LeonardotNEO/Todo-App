14366b815d886fef9ae322b58ba76695d02eb147
Omar Tveiten Sheikh <omarts@stud.ntnu.no>
2021-04-27 13:27:56 +0200

    fixed javadocs for notification service, and removed a unused methode

--- src/main/java/ntnu/idatt1002/service/NotificationService.java
+++ src/main/java/ntnu/idatt1002/service/NotificationService.java
@@ -9,12 +9,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
- * A class which provides some necessary features which utilises notification-data
+ * A class which provides some necessary features which utilises notification-data.
  */
 public class NotificationService {
 
     /**
-     * Communicates with NotificationDAO to fetch notifications for current user
+     * Communicates with NotificationDAO to fetch notifications for current user.
      * @return
      */
     public static ArrayList<Notification> getNotificationsByCurrentUser(){
@@ -22,7 +22,7 @@ public class NotificationService {
     }
 
     /**
-     * Method for getting the active notifications by current user
+     * Method for getting the active notifications by current user.
      * @return
      */
     public static ArrayList<Notification> getActiveNotificationsByCurrentUser(){
@@ -38,7 +38,7 @@ public class NotificationService {
     }
 
     /**
-     * Get all not checked and active notifications from current user
+     * Get all not checked and active notifications from current user.
      * @return
      */
     public static ArrayList<Notification> getActiveAndNotCheckedNotifications(){
@@ -54,7 +54,7 @@ public class NotificationService {
     }
 
     /**
-     * Communicates with NotificationDAO to add new notification to current user
+     * Communicates with NotificationDAO to add new notification to current user.
      * @param title
      * @param description
      */
@@ -64,25 +64,11 @@ public class NotificationService {
     }
 
     /**
-     * When using a setter method on a notification, we need to user this editNotification method on that notification in order to serialize it to storage
+     * When using a setter method on a notification, we need to user this editNotification method on that notification in order to serialize it to storage.
      * @param notification
      */
     public static void editNotification(Notification notification){
         NotificationDAO.serialize(notification);
-    }
-
-    /**
-     * Setting the checked variable in notification specified by id to true
-     * @param id
-     * @return boolean if the process was successful
-     * @throws IOException
-     */
-    public static boolean checkNotification(int id) throws IOException {
-        Notification notification = NotificationDAO.deserialize(UserStateService.getCurrentUserUsername(), id);
-        notification.setChecked(true);
-        NotificationDAO.serialize(notification);
-
-        return true;
     }
 
     /**
