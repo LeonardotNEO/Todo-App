c69ac034728ae6d07e22152bf6ea097340b83c97
saschrad <saschrad@stud.ntnu.no>
2021-04-27 19:11:44 +0200

    Added imageServiceTests

--- src/test/java/ntnu/idatt1002/service/ImageServiceTest.java
+++ src/test/java/ntnu/idatt1002/service/ImageServiceTest.java
@@ -1,4 +1,26 @@
 package ntnu.idatt1002.service;
 
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ImageServiceTest {
+
+    @Test
+    public void addAndGetImageWithCurrentUserTest() {
+        File file = new File("src/main/resources/images/logo.PNG");
+
+        boolean ran = false;
+        try {
+            ImageService.addImageToCurrentUser(file);
+            ran = true;
+        } catch (IOException e) {}
+        assertTrue(ran);
+
+        assertEquals("src\\main\\resources\\saves\\Test\\Images\\logo.png", ImageService.getAllImagesCurrentUser().get(0).getPath());
+    }
 }
